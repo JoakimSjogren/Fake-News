08ebf2549d16f89c264f5fcb208f2a1bc3719108
JoakimSjogren <70525815+JoakimSjogren@users.noreply.github.com>
2020-10-20 12:19:20 +0200

    Added article layout
    foreach loop to print articles

--- data.php
+++ data.php
@@ -4,5 +4,9 @@ declare(strict_types=1);
 
 // This is the file where you can keep your data arrays such as articles and
 // authors.
-$article = [];
+$articles = [
+    ['title' => 'Title1', 'content' => 'Content', 'author' => 'firstname lastname', 'publishedDate' => '2020/01-01', 'likeCounter' => '1'],
+    ['title' => 'Title2', 'content' => 'Content', 'author' => 'firstname lastname', 'publishedDate' => '2020/01-01', 'likeCounter' => '1'],
+    ['title' => 'Title3', 'content' => 'Content', 'author' => 'firstname lastname', 'publishedDate' => '2020/01-01', 'likeCounter' => '1']
+];
 $author = [];
--- index.php
+++ index.php
@@ -4,9 +4,30 @@
 // keep as much logic out of the HTML as possible. Put the PHP logic in the top
 // of the files containing HTML or even better; in another PHP file altogether.
     require __DIR__ .'/header.php';
+    require __DIR__ .'/data.php';
 ?>
 
 <h1 class = "title">fake news</h1>
+
+<?php
+foreach($articles as $article) {
+    $title = $article['title'];
+    $content = $article['content'];
+    $author = $article['author'];
+    $date = $article['publishedDate'];
+    $likes = $article['likeCounter'];
+
+    ?>
+        <!-- Conent -->
+        <h1> <?php echo $title ?> </h1>
+        <h1> <?php echo $content ?> </h1>
+        <h1> <?php echo $author ?> </h1>
+        <h1> <?php echo $date ?> </h1>
+        <h1> <?php echo $likes ?> </h1>
+    <?php
+
+}
+?>
 </body>
 
 </html>
